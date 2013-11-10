2c9ef52c5c29c1ae507c407567280ef6c7b5a82f
ADStrovers <ADStrovers@gmail.com>
2013-11-10 15:53:42 -0600

    Rawr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2431dbfecd01d7cc1229565a942b2904846febec93d17fc8e58961d380ec8040ae5ac2783b5964b3bcb0dd36efd29c4630b51c8b7670e464ca0d46930f1e701a'
